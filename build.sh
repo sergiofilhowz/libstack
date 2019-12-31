1cbac2e8bc9212c40fbbcd255d9ad37dcd0d3883
Sergio Marcelino <sergiofilhow@gmail.com>
2019-12-31 20:17:09 -0300

    Fixing build shell script to install yarn dependencies before yarn build

--- build.sh
+++ build.sh
@@ -3,31 +3,50 @@ EXIT_STATUS=0
 echo "Building Workspace"
 yarn install || EXIT_STATUS=$?
 
+
 echo "Adding TypeScript compiler globally"
 yarn global add typescript
 
-echo "Building @libstack/data"
+
 cd packages/data
+echo "Installing @libstack/data"
+yarn || EXIT_STATUS=$?
+echo "Building @libstack/data"
 yarn build || EXIT_STATUS=$?
 
-echo "Building @libstack/keycloak"
+
 cd ../keycloak
+echo "Installing @libstack/keycloak"
+yarn || EXIT_STATUS=$?
+echo "Building @libstack/keycloak"
 yarn build || EXIT_STATUS=$?
 
-echo "Building @libstack/router"
+
 cd ../router
+echo "Installing @libstack/router"
+yarn || EXIT_STATUS=$?
+echo "Building @libstack/router"
 yarn build || EXIT_STATUS=$?
 
-echo "Building @libstack/sequel"
+
 cd ../sequel
+echo "Installing @libstack/sequel"
+yarn || EXIT_STATUS=$?
+echo "Building @libstack/sequel"
 yarn build || EXIT_STATUS=$?
 
-echo "Building @libstack/server"
+
 cd ../server
+echo "Installing @libstack/server"
+yarn || EXIT_STATUS=$?
+echo "Building @libstack/server"
 yarn build || EXIT_STATUS=$?
 
-echo "Building @libstack/tester"
+
 cd ../tester
+echo "Installing @libstack/tester"
+yarn || EXIT_STATUS=$?
+echo "Building @libstack/tester"
 yarn build || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
